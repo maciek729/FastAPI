2684a896decce16f5a105fe8b13cbe1082d88a14
WikiS <61358557+kiwimamba@users.noreply.github.com>
2025-06-06 15:43:21 +0200

    database connection

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.yltbfinwwpmzrcnmxsvd:r1FLoSj2ifSxvQcJ@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
