6d805667c8ba01025ac609700897aac3d2398611
Oktawik <oktawianpolak@gmail.com>
2025-06-08 10:19:30 +0200

    Naprawa bledow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
-
+        
       </Routes>
     </BrowserRouter>
   );
