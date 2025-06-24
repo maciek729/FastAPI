07da385841ecd7a65d1b6f7cf292f0c8004493c2
Julia <trzeciakiewicz.julia@gmail.com>
2025-06-24 19:33:07 +0200

    Style do chata, logowania, dodanie intra, style się troche rozjechały ale poprawie poten, z logikom jest wszytstko ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 import Dashboard from "./components/Dashboard"; 
 import Chat from "./components/Chat";
+import Intro from "./components/Intro";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/chat" element={<Chat/>} />
-        
+        <Route path="/intro" element={<Intro/>}/>        
       </Routes>
     </BrowserRouter>
   );
